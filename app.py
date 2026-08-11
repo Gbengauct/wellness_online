eb525fd02b388cf76fd1410079b5a6762e6ce0d5
Gbengauct <toyesegbenga6@gmail.com>
2026-08-11 11:13:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ import os
 app = Flask(__name__)
 
 # 🔑 PASTE YOUR GOOGLE GEMINI API KEY HERE
-GEMINI_API_KEY = "AIzaSyD5x..."  # <-- PASTE YOUR ACTUAL KEY HERE
-
+GEMINI_API_KEY =  url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
 @app.route('/')
 def home():
     return render_template('index.html')
